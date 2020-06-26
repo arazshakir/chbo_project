65f3c44e5ec6bf03e57d87cb3cf2d860fe348903
arazhakir <arazshakir@gmail.com>
2020-06-26 18:49:40 +0500

    all files done

--- Chbo/settings.py
+++ Chbo/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -136,3 +138,16 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+#email
+# EMIAL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 465
+# EMAIL_HOST_USER= 'arazshakir@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Nanodegree1@'
+# EMAIL_USE_TLS= True
