1793323863d5f58cb369fc0f101ff00832f9a866
arazhakir <arazshakir@gmail.com>
2020-06-27 18:37:47 +0500

    Added local settings

--- Chbo/settings.py
+++ Chbo/settings.py
@@ -151,3 +151,8 @@ MESSAGE_TAGS = {
 # EMAIL_HOST_USER= 'arazshakir@gmail.com'
 # EMAIL_HOST_PASSWORD = 'Nanodegree1@'
 # EMAIL_USE_TLS= True
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
